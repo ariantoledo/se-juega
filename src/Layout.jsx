bb212a222e09ad18fad31cf689f364329999b3de
Arian Toledo <ariantoledo83@gmail.com>
2026-03-23 03:50:09 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { createPageUrl } from "@/utils";
 import { base44 } from "@/api/base44Client";
 import { Home, PlusCircle, CalendarDays, User, Menu, X, Sun, Moon, MapPin, ArrowLeft, HelpCircle } from "lucide-react";
