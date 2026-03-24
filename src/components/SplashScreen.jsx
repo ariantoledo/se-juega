a7a6b3d5a8dcf107d55adec4bb1e78836b6509c1
Arian Toledo <ariantoledo83@gmail.com>
2026-03-24 21:11:54 +0000

    File changes

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -7,18 +7,4 @@ export default function SplashScreen({ onDone }) {
   }, [onDone]);
 
   return <div style={{ backgroundColor: "black", width: "100%", height: "100vh" }} />;
-}) {
-  useEffect(() => {
-    const timer = setTimeout(onDone, 700);
-    return () => clearTimeout(timer);
-  }, [onDone]);
-
-  return (
-    <motion.div
-      initial={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-      className="fixed inset-0 z-[100] bg-black"
-    />
-  );
 }
